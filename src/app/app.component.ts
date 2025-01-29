2286b321553c6dbbfabde865dec5a6ed4c9a67b3
Márcio J. Silva <105254896+ImMarcio@users.noreply.github.com>
2025-01-29 19:51:29 -0300

    feat: Implementação do footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { MainComponent } from './components/main/main.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, MainComponent],
+  imports: [RouterOutlet, NavbarComponent, MainComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
