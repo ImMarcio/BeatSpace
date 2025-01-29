fedaa5ee2f888f21a6380e6a0ee1cc22e8a55320
Márcio J. Silva <105254896+ImMarcio@users.noreply.github.com>
2025-01-29 20:58:36 -0300

    build: Finalizando a instalação do primeng

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { FormsModule } from '@angular/forms';
+import { FloatLabelModule } from 'primeng/floatlabel';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [ButtonModule,FloatLabelModule,FormsModule ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+value: any;
 
 }
